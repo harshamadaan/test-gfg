34d320725bd106354602acd5543e38f00544a66b
harsha arora <harshamadaan50@gmail.com>
2026-01-30 13:33:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,29 +1,38 @@
+import java.util.*;
+
 class Solution {
-    int[][] t;
+
+    static int[][] t;
+
     public int knapsack(int W, int val[], int wt[]) {
-        // code here
-        int n=wt.length;
-        t=new int[n+1] [W+1];
-        for(int i=0;i<=n;i++){
-            Arrays.fill(t[i],-1);
-            
+
+        int n = wt.length;
+        t = new int[n + 1][W + 1];
+
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(t[i], -1);
         }
-        return solve(wt,val,W,n);
-        
+
+        return solve(W, val, wt, n);
     }
-    public int solve(int [] wt,int[] val,int W,int n){
-        if(n==0||W==0)
-        return 0;
-        //memorisation check
-        if(t[n][W]!=-1)
-        return t[n][W];
-        
-        if(wt[n-1]<=W){
-            t[n][W]=Math.max(val[n-1]+solve(wt,val,W-wt[n-1],n-1),solve(wt,val,W,n-1));
-            
-        } else{
-            t[n][W]=solve(wt,val,W,n-1);
+
+    static int solve(int W, int val[], int wt[], int n) {
+
+        if (n == 0 || W == 0)
+            return 0;
+
+        if (t[n][W] != -1)
+            return t[n][W];
+
+        if (wt[n - 1] <= W) {
+            t[n][W] = Math.max(
+                    val[n - 1] + solve(W - wt[n - 1], val, wt, n - 1),
+                    solve(W, val, wt, n - 1)
+            );
+        } else {
+            t[n][W] = solve(W, val, wt, n - 1);
         }
+
         return t[n][W];
     }
 }
