86e88c1bdc9832fd53932e1f9ceaa9380c62c009
harsha arora <harshamadaan50@gmail.com>
2026-06-28 23:31:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimum sum partition/minimum-sum-partition.java
+++ Difficulty: Hard/Minimum sum partition/minimum-sum-partition.java
@@ -1,22 +1,21 @@
-// User function Template for Java
-
 class Solution {
-
+    boolean[][] t;
     public int minDifference(int arr[]) {
+        // code here
         int n=arr.length;
         int range=0;
-        for(int x: arr) range+=x;
-        
-        //initalize
-        boolean[][] t=new boolean[n+1][range+1];
-        for(int i=0;i<=n;i++)
-        t[i][0]=true;
-        for(int j=1;j<=range;j++)
-        t[0][j]=false;
-        
-        //dp table
+        for(int x:arr){
+            range+=x;
+        }
+        t=new boolean[n+1][range+1];
+        for(int i=0;i<=n;i++){
+            t[i][0]=true;
+        }
+        for(int j=1;j<=range;j++){
+            t[0][j]=false;
+        }
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=range;j++){
+            for(int j=0;j<=range;j++){
                 if(arr[i-1]<=j){
                     t[i][j]=t[i-1][j-arr[i-1]] || t[i-1][j];
                 }else{
@@ -24,7 +23,6 @@ class Solution {
                 }
             }
         }
-        //find min diff
         int min=Integer.MAX_VALUE;
         for(int s1=0;s1<=range/2;s1++){
             if(t[n][s1]){
@@ -32,8 +30,6 @@ class Solution {
             }
         }
         return min;
-        
-        
         
     }
 }
