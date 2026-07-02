96ae38bce19734ea21e85359b011b7111bfac1d1
harsha arora <harshamadaan50@gmail.com>
2026-07-02 16:19:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -1,26 +1,32 @@
 class Solution {
-    static int [][] t=new int[1001][1001];
+    static int[][]t;
     static int matrixMultiplication(int arr[]) {
         // code here
-        int N=arr.length;
-        for(int i=0;i<1001;i++){
-            for(int j=0;j<1001;j++){
+        int n=arr.length;
+        t=new int[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
                 t[i][j]=-1;
             }
         }
-        return solve(arr,1,N-1);
+        return solve(arr,1,n-1);
+        
         
     }
-    static int solve(int[]arr,int i,int j){
-        //base case
+    static int solve(int[] arr,int i,int j){
+        //base
         if(i>=j) return 0;
         if(t[i][j]!=-1) return t[i][j];
+        
         int min=Integer.MAX_VALUE;
         for(int k=i;k<j;k++){
-            int temp=solve(arr,i,k)+solve(arr,k+1,j)+arr[i-1]*arr[k]*arr[j];
-            min=Math.min(min , temp);
+            int temp=solve(arr,i,k)+solve(arr,k+1,j)+(arr[i-1]*arr[k]*arr[j]);
+            
+            if(temp<min){
+                min=temp;
+            }
+            
         }
         return t[i][j]=min;
-        
     }
 }
